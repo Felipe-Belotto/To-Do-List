1766b04da7cd296920fbfe9899af546897bd3643
Felipe-Belotto <felipeeduardo817@gmail.com>
2023-12-29 01:22:35 -0300

    adicionando função de adicionar tarefa e de apagar

--- src/assets/components/Coluna/Coluna.jsx
+++ src/assets/components/Coluna/Coluna.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Coluna.module.css';
 import { Reorder, useDragControls } from "framer-motion"
 import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
@@ -11,6 +11,27 @@ function Coluna(props) {
   
   const controls = useDragControls()
 
+  const [listaTarefas, setListaTarefas] = useState([])
+
+  const [novaTarefa, setNovaTarefa] = useState([])
+
+  function adicionaTarefa() {
+
+    if (!listaTarefas.includes(novaTarefa)) {
+      setListaTarefas((novaLista) => [...novaLista, novaTarefa]);
+      setNovaTarefa("")
+    } else {
+     alert("Tarefa ja existe")
+     setNovaTarefa("")
+    }
+
+  }
+  
+  function deletaTarefa(indexToDelete) {
+    setListaTarefas((prevLista) => prevLista.filter((_, i) => i !== indexToDelete));
+  }
+  
+  
   return (
 
     <Reorder.Item key={props.item} value={props.item}  dragListener={false} dragControls={controls}>
@@ -30,13 +51,17 @@ function Coluna(props) {
 
     
     <div className={styles.nova__tarefa}>
-    <InputNovaTarefa />
-    <Button onClick={()=> {alert("ta funcionando")}} style={{display: "flex", height:"30px", backgroundColor:"#b3b3b347", border:"none", alignItems:"center", borderRadius:"0"}}>
+    <InputNovaTarefa value={novaTarefa} onChange={(e) => setNovaTarefa(e.target.value)} />
+    <Button onClick={adicionaTarefa} style={{display: "flex", height:"30px", backgroundColor:"#b3b3b347", border:"none", alignItems:"center", borderRadius:"0"}}>
     <AddCircleSharpIcon style={{ color: 'darkgray', fontSize:"20px"}} />
     </Button>
     </div>
 
- 
+
+    {listaTarefas.map((tarefa, index) => (
+   <Tarefa key={index} nome={tarefa} onDelete={() => deletaTarefa(index)} />
+    ))}
+
 
 
     </section>
--- src/assets/components/Tarefa/Tarefa.jsx
+++ src/assets/components/Tarefa/Tarefa.jsx
@@ -1,29 +1,30 @@
-import React, { useState } from 'react';
-import styles from './Tarefa.module.css';
-import { Button, IconButton } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-import DoneIcon from '@mui/icons-material/Done';
+  import React, { useState } from 'react';
+  import styles from './Tarefa.module.css';
+  import { Button, IconButton } from '@mui/material';
+  import DeleteIcon from '@mui/icons-material/Delete';
+  import DoneIcon from '@mui/icons-material/Done';
 
-function Tarefa(props) {
+  function Tarefa(props) {
 
-  const[status, setStatus] = useState(false)
+    const[status, setStatus] = useState(false)
 
-  return (
-    <div className={styles.tarefa} 
-    style={{backgroundColor: status ? "rgba(24, 24, 24, 0.316)" : "rgba(207, 207, 207, 0.316)", 
-    textDecoration:status ? "line-through": "none",
+    return (
+      <div className={styles.tarefa} 
+      style={{backgroundColor: status ? "rgba(24, 24, 24, 0.316)" : "rgba(207, 207, 207, 0.316)", 
+      textDecoration:status ? "line-through": "none",
 
-    }}>
+      }}>
 
-    <IconButton className={styles.botao} aria-label="done" size="small" onClick={()=>{setStatus(!status)}}>
-    <DoneIcon fontSize="small" />
-    </IconButton>
-    <p>Fazer a To-Do-List</p>
-    <IconButton className={styles.botao} aria-label="delete" size="small">
-    <DeleteIcon fontSize="small" />
-    </IconButton>
-    </div>
-  );
-}
+      <IconButton className={styles.botao} aria-label="done" size="small" onClick={()=>{setStatus(!status)}}>
+      <DoneIcon fontSize="small" />
+      </IconButton>
+      <p>{props.nome}</p>
+      
+      <IconButton className={styles.botao} aria-label="delete" size="small" onClick={props.onDelete}>
+      <DeleteIcon fontSize="small"  />
+      </IconButton>
+      </div>
+    );
+  }
 
-export default Tarefa;
+  export default Tarefa;
